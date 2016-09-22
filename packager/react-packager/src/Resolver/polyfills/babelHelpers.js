545a11aa2eda26652c65ea341b279a6812a05cc9
Gustavo Siñovsky <g.sinovsky@gmail.com>
2016-09-22 09:12:11 -0430

    Fix babel bug https://github.com/facebook/react-native/issues/5747#issuecomment-192356656

--- packager/react-packager/src/Resolver/polyfills/babelHelpers.js
+++ packager/react-packager/src/Resolver/polyfills/babelHelpers.js
@@ -227,3 +227,9 @@ babelHelpers.toConsumableArray = function (arr) {
     return Array.from(arr);
   }
 };
+
+babelHelpers.typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
+  return typeof obj;
+} : function (obj) {
+  return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj;
+};
